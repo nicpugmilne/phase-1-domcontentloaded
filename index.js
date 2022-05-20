ededf9c4e4c7747d89f1a98bb7a9098befcf2f87
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-20 11:10:03 -0600

    commit tree babfd96f443be5ab099ef911e76621449ef73c9e

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+
+const textElement = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", () => textElement.textContent = "This is really cool!")
